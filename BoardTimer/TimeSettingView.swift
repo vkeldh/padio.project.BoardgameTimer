d13e2122cd736be91cd4b953afd79dfa5fa36d56
vkeldh <vkeldh.project@gmail.com>
2018-07-18 16:57:35 +0900

    first commit

---
+++ BoardTimer/TimeSettingView.swift
@@ -0,0 +1,9 @@
+//
+//  TimeSettingView.swift
+//  BoardTimer
+//
+//  Created by 파디오 on 13/07/2018.
+//  Copyright © 2018 파디오. All rights reserved.
+//
+
+import Foundation
---
+++ BoardTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoardTimer
+//
+//  Created by 파디오 on 13/07/2018.
+//  Copyright © 2018 파디오. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
